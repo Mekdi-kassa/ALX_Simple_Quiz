b202d7a41653726493035517991dbfc9f5061865
Mekdelawit Abadian <mekdelawitkassa6@gmail.com>
2024-10-03 13:56:49 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,14 +1,21 @@
-document.getElementById('submit-answer').addEventListener('click', checkAnswer);
-function checkAnswer(){
-    let correctAnswer="4"
-    const radios=document.getElementsByName('quiz')
-    for(let i=0;i<radios.length;i++){
-        if(radios[i].checked==correctAnswer){
-            document.getElementById('feedback').innerText="Correct! Well done";
+        document.getElementById('submitBtn').addEventListener('click', checkAnswer);
+
+        function checkAnswer() {
+            let correctAnswer = "4";
+            // Use querySelector to get the checked radio button
+            const checkedRadio = document.querySelector('input[name="quiz"]:checked');
+            
+            if (checkedRadio) {
+                // Retrieve the user's answer from the checked radio button
+                let userAnswer = checkedRadio.value;
+
+                // Check if the user's answer is correct
+                if (userAnswer === correctAnswer) {
+                    document.getElementById('feedback').innerText = "Correct! Well done.";
+                } else {
+                    document.getElementById('feedback').innerText = "That's incorrect. Try again!";
+                }
+            } else {
+                document.getElementById('feedback').innerText = "Please select an answer.";
+            }
         }
-        else{
-            document.getElementById('feedback').innerText="That's incorrect. Try again!"
-        }
-    }
-    
-}
